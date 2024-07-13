1762a129a6e60c8c6cf82e00fae4ed404db10853
natia shonia <nat.shonia98@gmail.com>
2024-07-13 23:50:09 +0400

    Add project files.

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => PrintStudentsDetails(),
+                () => PrintEmployeeDetails()
+
+
+                );
+
+            Console.ReadLine();
+        }
+
+        private static void PrintEmployeeDetails()
+        {
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("From Employee");
+        }
+
+        private static void PrintStudentsDetails()
+        {
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+        }
+    }
+}
---
+++ SingletonDemo/singletondemo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        private static readonly object obj = new object();
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+
+                    lock (obj)
+                    {
+                        if (instance == null) //აქ ამოწმებს საერთოდ შექმნილია თუ არა ინსტანსი
+                            instance = new Singleton();
+
+                    }
+                }
+
+
+                return instance;
+
+            }
+        }
+
+
+
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter value-ctor" + counter.ToString());
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+
+    }
+
+}
